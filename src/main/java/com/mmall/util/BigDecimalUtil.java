0cf77a83d29f8823d6f14c536e5baf7f84ce9dd6
BaiWeiJieKu <2534964002@qq.com>
2019-09-07 21:06:52 +0800

    购物车模块完成

---
+++ src/main/java/com/mmall/util/BigDecimalUtil.java
@@ -0,0 +1,67 @@
+package com.mmall.util;
+
+import java.math.BigDecimal;
+
+/**价格运算工具（防止丢失精度）
+ * @author QinFen
+ * @date 2019/9/7 0007 17:08
+ */
+public class BigDecimalUtil {
+
+    private BigDecimalUtil(){
+
+    }
+
+
+    /**加法运算
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static BigDecimal add(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.add(b2);
+    }
+
+    /**减法运算
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static BigDecimal sub(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.subtract(b2);
+    }
+
+
+    /**乘法运算
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static BigDecimal mul(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2);
+    }
+
+    /**除法运算
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static BigDecimal div(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.divide(b2,2,BigDecimal.ROUND_HALF_UP);//四舍五入,保留2位小数
+
+        //除不尽的情况
+    }
+
+
+
+
+
+}
